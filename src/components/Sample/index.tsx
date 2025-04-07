56b6536822ede9f701981518c127ffee14f5c6d1
Hiroto-Shida <uecshida6295@gmail.com>
2025-04-08 00:22:10 +0900

    雑にサンプルコンポーネント

--- src/components/Sample/index.tsx
+++
@@ -1,9 +0,0 @@
-const Sample = () => {
-  return (
-    <div>
-      <h1 className="font-bold">Welcome to the Sample Page</h1>
-      <p>This is the Sample page of our application.</p>
-    </div>
-  );
-};
-export default Sample;
