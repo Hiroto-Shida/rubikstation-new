8a4820be253e6f6668e4274ad2e033bf9045cb2e
Hiroto-Shida <uecshida6295@gmail.com>
2025-04-10 18:48:11 +0900

    npm run formatでformatされてなかったファイルをformat

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
 	return (
 		<div>
-			<h1 className="font-bold">Welcome to the Home Page</h1>
+			<h1 className='font-bold'>Welcome to the Home Page</h1>
 			<p>This is the home page of our application.</p>
 		</div>
 	);
