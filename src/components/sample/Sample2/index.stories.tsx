4d6fc9c79b33ead93a98dcecf03451ac11445f19
INSERT-io <isomanatsuki@team-lab.com>
2025-05-15 20:39:19 +0900

    階層をSampleに移動

--- src/components/sample/Sample2/index.stories.tsx
+++ src/components/sample/Sample2/index.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import Sample2 from './index';
 
 export default {
-	title: 'Components/Sample2',
+	title: 'Components/Sample/Sample2',
 	component: Sample2,
 } as Meta<typeof Sample2>;
 
